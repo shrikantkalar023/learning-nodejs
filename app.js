be010e13d754260dbe523aea17a8dca35c19878a
Shrikant Kalar <shrikantkalar023@gmail.com>
2023-12-14 01:47:55 +0530

    File System module

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-const os = require("node:os");
+const fs = require("node:fs");
 
-console.log(os.platform());
-console.log(os.homedir());
-console.log(os.hostname());
-console.log(os.arch());
-console.log(os.cpus());
-console.log(os.freemem());
-console.log(os.totalmem());
-console.log(os.uptime());
-console.log(os.userInfo());
-console.log(os.version());
-console.log(os.release());
-console.log(os.type());
-console.log(os.networkInterfaces());
-console.log(os.loadavg());
+fs.readdir("./", (err, files) => {
+  if (err) console.log(err);
+  else console.log(files);
+});
