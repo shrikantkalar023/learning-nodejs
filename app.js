00cb3ffea92c641496562e46e8283ea5f9bd86e6
Shrikant Kalar <shrikantkalar023@gmail.com>
2023-12-14 01:33:32 +0530

    OS module

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const path = require("node:path");
+const os = require("node:os");
 
-console.log(path.parse(__filename));
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.uptime());
+console.log(os.userInfo());
+console.log(os.version());
+console.log(os.release());
+console.log(os.type());
+console.log(os.networkInterfaces());
+console.log(os.loadavg());
