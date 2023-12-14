17432896cea5e6e30d8e6c4f6b2be6ea39881b81
Shrikant Kalar <shrikantkalar023@gmail.com>
2023-12-14 14:37:40 +0530

    event arguments

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const EventEmitter = require("node:events");
-
+const log = require("./logger");
 // Create an instance of the EventEmitter class
 const emitter = new EventEmitter();
 
 // Listener for the below event
-emitter.on("messageLogged", () => {
-  console.log("Listener called");
+emitter.on("messageLogged", (eventArg) => {
+  console.log("Listener called", eventArg);
 });
 
 // Raise an event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
+
+log("signal");
--- logger.js
+++ logger.js
@@ -1,11 +1,17 @@
-console.log(__filename);
-console.log(__dirname);
+const EventEmitter = require("node:events");
+
+const emitter = new EventEmitter();
+
 let url = "http://mylogger.io/log";
 
+emitter.on("logging", (eventArg) => {
+  console.log("Logging Listener called", eventArg);
+});
+
 const log = (message) => {
+  emitter.emit("logging", { data: message });
   // Send an HTTP request
   console.log(message);
 };
 
 module.exports = log;
-// can export an obj or a fn
