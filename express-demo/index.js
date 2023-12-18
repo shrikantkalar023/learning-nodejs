b12d5537d378ea5b16640e641b63ba2c8c88ef95
Shrikant Kalar <shrikantkalar023@gmail.com>
2023-12-18 13:25:45 +0530

    built-in middlewares

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,6 +4,8 @@ const logger = require("./logger");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(logger);
 
