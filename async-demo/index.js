2d00e9cb5bcfbf91eb5fdf6618874bb68416e5f2
Shrikant Kalar <shrikantkalar023@gmail.com>
2023-12-21 02:36:46 +0530

    Promises instead of Callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,35 +1,45 @@
-const getUser = (id, callback) => {
-  setTimeout(() => {
-    console.log("Reading a user from a database...");
-    callback({ id: id, name: "shrikant" });
-  }, 2000);
-};
-
-const getRepos = (userName, callback) => {
-  setTimeout(() => {
-    console.log(`Reading repositories owned by ${userName} from github...`);
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
-};
-
-// Asyncronous
-console.log("Before");
-getUser(1, (user) => {
-  getRepos(user.name, (repos) => {
-    console.log(repos);
-    getCommits(repos[0], (commits) => {
-      console.log(commits);
-      // Callback Hell
-      // Nested Callbacks
-      // Christmas tree problem
-    });
+const getUser = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      resolve({ id: id, name: "shrikant" });
+      // reject(new Error("Failed to get user from database"));
+    }, 2000);
   });
-});
-console.log("After");
+};
 
-// Syncronous
+const getRepos = (userName) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Reading repositories owned by ${userName} from github...`);
+      // resolve(["repo1", "repo2", "repo3"]);
+      reject(new Error("Failed to get repos from github"));
+    }, 2000);
+  });
+};
+
+const getCommits = (repo) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Reading commits in ${repo} from github...`);
+      resolve(["commit1", "commit2", "commit3"]);
+      // reject(new Error(`Failed to get commits from ${repo}`));
+    }, 2000);
+  });
+};
+
 console.log("Before");
-const user = getUser(1);
-const repos = getRepos(user.name);
-const commits = getCommits(repos[0]);
+// getUser(1, (user) => {
+//   getRepos(user.name, (repos) => {
+//     console.log(repos);
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     });
+//   });
+// });
+getUser(1)
+  .then((user) => getRepos(user.name))
+  .then((repos) => getCommits(repos[0]))
+  .then((commits) => console.log(commits))
+  .catch((err) => console.log(err.message));
 console.log("After");
