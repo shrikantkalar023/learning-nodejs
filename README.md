2547dc4f70521d87f4b822ec72e64846657fe0c4
Shrikant Kalar <shrikantkalar023@gmail.com>
2023-12-21 01:32:13 +0530

    Async callback Pattern

--- README.md
+++ README.md
@@ -53,3 +53,5 @@ This is the repo i created and updated while learning Node. **-Shrikant Kalar**
 23. For every api endpoint we should have a separate file/module.
 
 24. **Asynchronous** is differnt from **Concurrent** or **Multi-threaded**. We only have a single thread in node.
+
+25. **Async Patterns** - Callbacks, Promises, Async/Await
--- async-demo/index.js
+++ async-demo/index.js
@@ -1,7 +1,20 @@
-console.log("Before");
+const getUser = (id, callback) => {
+  setTimeout(() => {
+    console.log("Reading a user from a database...");
+    callback({ id: id, name: "shrikant" });
+  }, 2000);
+};
 
-// schedules a task to be performed in the future.
-setTimeout(() => {
-  console.log("Reading a user from a database...");
-}, 2000);
+const getRepos = (userName, callback) => {
+  setTimeout(() => {
+    console.log(`Reading repositories owned by ${userName} from github...`);
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+};
+
+console.log("Before");
+getUser(1, (user) => {
+  console.log(user);
+  getRepos(user.name, (repos) => console.log(repos));
+});
 console.log("After");
