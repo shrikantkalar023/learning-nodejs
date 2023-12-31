f016a995d968078b8c15b294c4d7e360eebb9f69
Shrikant Kalar <shrikantkalar023@gmail.com>
2023-12-31 14:44:12 +0530

    Array of sub documents

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -17,17 +17,14 @@ const Course = mongoose.model(
   "Course",
   new mongoose.Schema({
     name: String,
-    author: {
-      type: authorSchema,
-      required: true,
-    },
+    authors: [authorSchema],
   })
 );
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name,
-    author,
+    authors,
   });
 
   const result = await course.save();
@@ -47,5 +44,27 @@ async function updateAuthor(courseId) {
   });
 }
 
-// createCourse("Node Course", new Author({ name: "Mosh" }));
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+}
+
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  author.deleteOne();
+  course.save();
+}
+
+// createCourse("Node Course", [
+//   new Author({ name: "Lallu" }),
+//   new Author({ name: "Shri" }),
+//   new Author({ name: "Juhee" }),
+// ]);
+
 // updateAuthor("65912925a15ef2904dcc64d4");
+
+// addAuthor("65912d9e3ac25cf97637ec84", new Author({ name: "Baba" }));
+
+removeAuthor("65912d9e3ac25cf97637ec84", "65912d9e3ac25cf97637ec82");
